6d811cfec348eed9899ea21d9545e3ad68d2b9b6
Nathália <nathalia.gramst@hotmail.com>
2022-07-26 15:30:54 -0300

    Componentização e layout da página inicial

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import "../assets/css/reset.css";
 import "../assets/css/index.css";
 import Header from "./Header";
+import Home from "./Home";
 
 export default function App() {
     return (
-        <Header />
-    )
+        <>
+            <Header />
+            <Home />
+        </>
+
+    );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,5 +5,5 @@ export default function Header() {
         <header>
             CINEFLEX
         </header>
-    )
+    );
 }
